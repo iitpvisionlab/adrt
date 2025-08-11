87dfcf7f7e3f89fa74bb9201050ecfe4e51bdf20
Arseniy Terekhin <senyai@gmail.com>
2025-08-11 13:21:05 +0300

    feat: simplify `non_recursive` function logic

--- adrtlib/include/adrtlib/non_recursive.hpp
+++ adrtlib/include/adrtlib/non_recursive.hpp
@@ -7,28 +7,17 @@ namespace adrt {
 struct ADRTTask {
   int start;
   int stop;
-  int size;
-  bool left_visited;
+  int size;  // stop - start
   int mid;
-  template <typename MidCallback>
-  ADRTTask(int start, int stop, MidCallback mid_callback)
-      : start{start},
-        stop{stop},
-        left_visited{false},
-        size{stop - start},
-        mid{start + mid_callback(size)} {
+  bool left_visited;
+  ADRTTask(int size, int start, int stop, int mid)
+      : start{start}, stop{stop}, size{size}, mid{mid}, left_visited{false} {
     A_NEVER(stop <= start);
+    // A_NEVER(size < 2);
   }
   ADRTTask() {};
-  template <typename MidCallback>
-  ADRTTask left(MidCallback mid_callback) const {
-    return ADRTTask(this->start, this->mid, mid_callback);
-  }
-
-  template <typename MidCallback>
-  ADRTTask right(MidCallback mid_callback) const {
-    return ADRTTask(this->mid, this->stop, mid_callback);
-  }
+  int size_left() { return this->mid - this->start; }
+  int size_right() { return this->stop - this->mid; }
 };
 
 struct ADRTTaskStack {
@@ -36,31 +25,35 @@ struct ADRTTaskStack {
   int size{};
   template <typename MidCallback>
   ADRTTaskStack(int size, MidCallback mid_callback) {
-    this->append(ADRTTask(0, size, mid_callback));
+    this->append(ADRTTask(size, 0, size, mid_callback));
   }
   void append(ADRTTask&& task) { this->stack[this->size++] = std::move(task); }
   // true - then no element left
   bool pop() { return --this->size == 0; }
   ADRTTask* top() {
     A_NEVER(this->size < 1 ||
-            this->size >= sizeof(this->stack) / sizeof(this->stack)[0]);
+            this->size >=
+                static_cast<int>(sizeof(this->stack) / sizeof(this->stack[0])));
     return &this->stack[this->size - 1];
   }
 };
 
 template <typename ApplyCallback, typename MidCallback>
-constexpr void non_recursive(int size, ApplyCallback apply,
-                             MidCallback mid_callback) {
-  ADRTTaskStack tasks_stack(size, mid_callback);
+[[gnu::always_inline]] inline void non_recursive(int size, ApplyCallback apply,
+                                                 MidCallback mid_callback) {
+  ADRTTaskStack tasks_stack(size, mid_callback(size));
 
   ADRTTask* task = tasks_stack.top();
   for (;;) {
     while (!task->left_visited) {
       if (task->size > 2) {
-        tasks_stack.append(task->left(mid_callback));
+        int const size_left = task->size_left();
+        int const mid_left = mid_callback(size_left);
+        tasks_stack.append(ADRTTask(size_left, task->start, task->mid,
+                                    task->start + mid_left));
         task = tasks_stack.top();
       } else {
-        task->left_visited = true;
+        break;
       }
     }
     if (task->size > 1) {
@@ -78,8 +71,11 @@ constexpr void non_recursive(int size, ApplyCallback apply,
     task = tasks_stack.top();
     if (!task->left_visited) {
       task->left_visited = true;
-      if (task->size > 2) {
-        tasks_stack.append(task->right(mid_callback));
+      int const size_right = task->size_right();
+      if (size_right > 1) {
+        int const mid_right = mid_callback(size_right);
+        tasks_stack.append(
+            ADRTTask(size_right, task->mid, task->stop, task->mid + mid_right));
         task = tasks_stack.top();
       }
     }
